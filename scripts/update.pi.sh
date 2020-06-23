ffc400abbcd5296c98d31e14c3100b94947e7fcd
Keith Welch <keith@paralogiktech.com>
2020-06-23 14:18:13 -0400

    Commit for 0.1 at 2020-06-23 14:18:10

--- scripts/update.pi.sh
+++ scripts/update.pi.sh
@@ -5,7 +5,15 @@ cd ~/ddojo_local
 # this version erases any client changes
 # which we'd prefer anyway
 git fetch origin
-git reset --hard origin/master
+
+newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+if [ "$newUpdatesAvailable" != "" ]; then
+	git reset --hard origin/master
+	echo "Updates applied"
+	~/ddojo_local/scripts/launch.pi.sh
+else
+	echo "No updates found"
+fi
 
 ### might need to think about doing it this way in the future
 # fetch changes, git stores them in FETCH_HEAD
