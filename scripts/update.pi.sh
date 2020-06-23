54f69eabac3d06bad478ca78ffe15b297278d0b3
Keith Welch <keith@paralogiktech.com>
2020-06-23 14:06:08 -0400

    Commit for 0.1 at 2020-06-23 14:06:05

---
+++ scripts/update.pi.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# your repository folder
+cd ~/ddojo_local
+
+# this version erases any client changes
+# which we'd prefer anyway
+git fetch origin
+git reset --hard origin/master
+
+### might need to think about doing it this way in the future
+# fetch changes, git stores them in FETCH_HEAD
+#git fetch
+## check for remote changes in origin repository
+#newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+#if [ "$newUpdatesAvailable" != "" ]
+#then
+#        # create the fallback
+#        git branch fallbacks
+#        git checkout fallbacks
+#        git add .
+#        git add -u
+#        git commit -m `date "+%Y-%m-%d"`
+#        echo "fallback created"
+#        git checkout master
+#        git merge FETCH_HEAD
+#        echo "merged updates"
+#else
+#        echo "no updates available"
+#fi
